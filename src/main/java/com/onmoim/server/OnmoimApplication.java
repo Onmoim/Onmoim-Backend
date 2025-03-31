c8d383cefb37997963f28805f0b0b6015faef226
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2025-03-31 21:38:35 +0900

    init: create project

---
+++ src/main/java/com/onmoim/server/OnmoimApplication.java
@@ -0,0 +1,13 @@
+package com.onmoim.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnmoimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnmoimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onmoim/server/OnmoimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onmoim.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnmoimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
