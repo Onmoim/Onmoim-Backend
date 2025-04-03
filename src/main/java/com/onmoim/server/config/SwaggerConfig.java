8910394562ca45233fec1d01cd4a9e51d5ec7ffc
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2025-04-04 01:51:21 +0900

    style: stylecheck 설정 후 적용

--- src/main/java/com/onmoim/server/config/SwaggerConfig.java
+++ src/main/java/com/onmoim/server/config/SwaggerConfig.java
@@ -12,18 +12,18 @@ import java.util.Arrays;
 @Configuration
 public class SwaggerConfig {
 
-    @Bean
-    public OpenAPI openAPI() {
-        Info info = new Info()
-                .version("v1.0") //버전
-                .title("Onmoim API") //이름
-                .description("온라인 소규모 모임 API"); //설명
-        return new OpenAPI()
-                .servers(Arrays.asList(
-                        new Server().url("http://localhost:8080").description("Local 서버"),
-                        new Server().url("https://onmoim.store").description("Production 서버")
-                ))
-                .info(info);
-    }
+	@Bean
+	public OpenAPI openAPI() {
+		Info info = new Info()
+			.version("v1.0") //버전
+			.title("Onmoim API") //이름
+			.description("온라인 소규모 모임 API"); //설명
+		return new OpenAPI()
+			.servers(Arrays.asList(
+				new Server().url("http://localhost:8080").description("Local 서버"),
+				new Server().url("https://onmoim.store").description("Production 서버")
+			))
+			.info(info);
+	}
 
 }
--- src/main/java/com/onmoim/server/example/controller/RedisController.java
+++ src/main/java/com/onmoim/server/example/controller/RedisController.java
@@ -11,23 +11,23 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/test/redis")
 public class RedisController {
 
-    private final RedisTemplate<String, String> redisTemplate;
+	private final RedisTemplate<String, String> redisTemplate;
 
-    @PostMapping
-    public ResponseEntity<String> create(@RequestParam(required = false) String value) {
-        if(!StringUtils.hasText(value)) {
-            return ResponseEntity.badRequest().body("value is empty");
-        }
-        redisTemplate.opsForValue().set("key", value);
-        return ResponseEntity.ok().body("ok");
-    }
+	@PostMapping
+	public ResponseEntity<String> create(@RequestParam(required = false) String value) {
+		if(!StringUtils.hasText(value)) {
+			return ResponseEntity.badRequest().body("value is empty");
+		}
+		redisTemplate.opsForValue().set("key", value);
+		return ResponseEntity.ok().body("ok");
+	}
 
-    @GetMapping
-    public ResponseEntity<String> get() {
-        String find = redisTemplate.opsForValue().get("key");
-        if(find == null) {
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok().body(find);
-    }
+	@GetMapping
+	public ResponseEntity<String> get() {
+		String find = redisTemplate.opsForValue().get("key");
+		if(find == null) {
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok().body(find);
+	}
 }
