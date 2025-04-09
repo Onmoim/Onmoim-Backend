103caac95340b37aea33af1c9583c7e57f9dfd4a
최윤정 <155123302+yunjeooong@users.noreply.github.com>
2025-04-09 11:46:42 +0900

    feat: 배포 환경에 Swagger UI 적용

--- src/main/java/com/onmoim/server/config/SwaggerConfig.java
+++ src/main/java/com/onmoim/server/config/SwaggerConfig.java
@@ -3,8 +3,11 @@ package com.onmoim.server.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
 
 @Configuration
 public class SwaggerConfig {
@@ -16,6 +19,10 @@ public class SwaggerConfig {
                 .title("Onmoim API") //이름
                 .description("온라인 소규모 모임 API"); //설명
         return new OpenAPI()
+                .servers(Arrays.asList(
+                        new Server().url("http://localhost:8080").description("Local 서버"),
+                        new Server().url("https://onmoim.store").description("Production 서버")
+                ))
                 .info(info);
     }
 
