40aa202ce0f38816e63e5ee220b86ff2557683b9
최윤정 <155123302+yunjeooong@users.noreply.github.com>
2025-04-30 09:54:22 +0900

    fix: FileValidator 빈 정의 및 설정 파일 추가

--- src/main/java/com/onmoim/server/config/FileValidatorConfig.java
+++ src/main/java/com/onmoim/server/config/FileValidatorConfig.java
@@ -1,4 +1,32 @@
 package com.onmoim.server.config;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
+
+import com.onmoim.server.common.s3.validator.DefaultFileValidator;
+import com.onmoim.server.common.s3.validator.FileValidator;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Configuration
 public class FileValidatorConfig {
+
+	@Value("${file.upload.max-size}")
+	private String maxFileSize;
+
+	@Value("${file.upload.allowed-types}")
+	private String allowedFileTypesStr;
+
+	@Bean
+	public FileValidator fileValidator() {
+		List<String> allowedFileTypes = Arrays.asList(allowedFileTypesStr.split(","));
+		log.info("FileValidator 빈 생성: 허용 타입 = {}, 최대 크기 = {}", allowedFileTypes, maxFileSize);
+		return new DefaultFileValidator(allowedFileTypes, DataSize.parse(maxFileSize));
+	}
 }
