e2a89f823564e332bcf3a8827465c079ef527e3a
dbkoo <elqlkoo@gmail.com>
2025-05-03 02:24:16 +0900

    feat: 임시 인증 처리를 위한 CustomOAuth2User, StubAuthenticationFilter 추가

---
+++ src/main/java/com/onmoim/server/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.onmoim.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.onmoim.server.security.StubAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig {
+
+	@Bean
+	@Profile("local")
+	public SecurityFilterChain localSecurityFilterChain(HttpSecurity http, StubAuthenticationFilter stubAuthenticationFilter) throws Exception {
+		return http
+			.csrf(csrf -> csrf.disable())
+			.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+			.addFilterBefore(stubAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+			.build();
+	}
+
+}
