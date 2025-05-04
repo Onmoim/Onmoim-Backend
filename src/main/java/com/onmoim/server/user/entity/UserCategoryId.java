fa44283a1be6a91993826cb4fb6cb53f0aec6f15
dbkoo <elqlkoo@gmail.com>
2025-05-04 18:33:40 +0900

    refactor: UserCategoryId 불변 객체로 리팩토링

--- src/main/java/com/onmoim/server/user/entity/UserCategoryId.java
+++ src/main/java/com/onmoim/server/user/entity/UserCategoryId.java
@@ -3,21 +3,25 @@ package com.onmoim.server.user.entity;
 import java.io.Serializable;
 
 import jakarta.persistence.Embeddable;
-import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Embeddable
 @Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 @EqualsAndHashCode
 public class UserCategoryId implements Serializable {
 
-	private Long userId;
-	private Long categoryId;
+	private final Long userId;
+	private final Long categoryId;
+
+	protected UserCategoryId() {
+		this.userId = null;
+		this.categoryId = null;
+	}
+
+	public UserCategoryId(Long userId, Long categoryId) {
+		this.userId = userId;
+		this.categoryId = categoryId;
+	}
 
 }
