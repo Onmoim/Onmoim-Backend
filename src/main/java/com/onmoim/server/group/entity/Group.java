52c9441cbbe306270f5d04ee4cb1fe2257e6e1a8
baejeu <zeus20240228@gmail.com>
2025-05-06 21:18:11 +0900

    feat: 모임(Group) 엔티티 생성
    - Group 엔티티 생성
    - GroupUser(모임-유저) 중간 엔티티 생성
    - GroupUserId 복합 키 생성
    - Status enum 생성: GroupUser 상태 표시
    - 서브 모듈 최신화

---
+++ src/main/java/com/onmoim/server/group/entity/Group.java
@@ -0,0 +1,51 @@
+package com.onmoim.server.group.entity;
+
+import org.hibernate.annotations.Comment;
+
+import com.onmoim.server.category.entity.Category;
+import com.onmoim.server.common.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "group")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Group extends BaseEntity {
+	@Id
+	@Column(name = "group_id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "category_id")
+	private Category category;
+	// todo 지역
+	@Comment("모임 이름")
+	private String name;
+
+	@Comment("모임 설명")
+	@Column(columnDefinition = "TEXT")
+	private String description;
+
+	@Comment("모임 최대 정원")
+	private int capacity;
+
+	@Comment("현재 모임 인원")
+	private int participantCount;
+
+	@Comment("모임 대표 사진")
+	private String imgUrl;
+
+}
