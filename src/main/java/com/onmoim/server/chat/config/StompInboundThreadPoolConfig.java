2e2c5b0b296945b7ee9fe6f5d387b060aa6735d6
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2025-05-19 18:46:17 +0900

    fix: stompInboundExecutor 설정 값 yaml 분리

--- src/main/java/com/onmoim/server/chat/config/StompInboundThreadPoolConfig.java
+++ src/main/java/com/onmoim/server/chat/config/StompInboundThreadPoolConfig.java
@@ -6,24 +6,28 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import lombok.RequiredArgsConstructor;
+
 /**
  * Inbound의 경우 특정 트래픽 이상은 메시지를 거절합니다. 스레드풀 조절과, 한계치를 초과했을 때 대응 방법은 고도화 대상으로 분류했습니다.
  */
 @Configuration
+@RequiredArgsConstructor
 public class StompInboundThreadPoolConfig {
+
+	private final StompInboundThreadProperties properties;
 
 	@Bean(name = "stompInboundExecutor")
 	public ThreadPoolTaskExecutor stompInboundExecutor() {
 		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
 		executor.setThreadNamePrefix("stomp-inbound-");
 		//추후 세부조정
-		executor.setCorePoolSize(4);
-		executor.setMaxPoolSize(20);
-		executor.setQueueCapacity(500);
-		executor.setKeepAliveSeconds(60);
-		executor.setRejectedExecutionHandler(new ThreadPoolExecutor.AbortPolicy());
-		executor.initialize();
+		executor.setCorePoolSize(properties.getCorePoolSize());
+		executor.setMaxPoolSize(properties.getMaxPoolSize());
+		executor.setQueueCapacity(properties.getQueueCapacity());
+		executor.setKeepAliveSeconds(properties.getKeepAliveSeconds());
 		executor.setRejectedExecutionHandler(new StompRejectedHandler());
+		executor.initialize();
 		return executor;
 	}
 }
---
+++ src/main/java/com/onmoim/server/chat/config/StompInboundThreadProperties.java
@@ -0,0 +1,17 @@
+package com.onmoim.server.chat.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+
+@Component
+@ConfigurationProperties(prefix = "stomp.inbound.thread")
+@Getter
+public class StompInboundThreadProperties {
+	private String namePrefix;
+	private int corePoolSize;
+	private int maxPoolSize;
+	private int queueCapacity;
+	private int keepAliveSeconds;
+}
