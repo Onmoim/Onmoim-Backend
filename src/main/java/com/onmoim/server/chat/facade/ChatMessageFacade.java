f1eb282f17e7986d5b42fb169de53fa8d2473cb7
‘seokjun7410’ <47974623+seokjun7410@users.noreply.github.com>
2025-06-05 22:34:20 +0900

    fix: ChatMessageDto, createGroup 수정
    1. ChatMessageDto groupId, chatUserDto 추가
    2. createGroup 에서 ChatRoomResponse 반환
    chore: google oauth client 변경

--- src/main/java/com/onmoim/server/chat/facade/ChatMessageFacade.java
+++ src/main/java/com/onmoim/server/chat/facade/ChatMessageFacade.java
@@ -4,8 +4,17 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.onmoim.server.chat.dto.ChatMessageDto;
+import com.onmoim.server.chat.dto.ChatUserDto;
 import com.onmoim.server.chat.service.ChatMessageService;
 import com.onmoim.server.chat.service.ChatRoomMemberQueryService;
+import com.onmoim.server.common.exception.CustomException;
+import com.onmoim.server.common.exception.ErrorCode;
+import com.onmoim.server.group.entity.Group;
+import com.onmoim.server.group.entity.GroupUser;
+import com.onmoim.server.group.implement.GroupQueryService;
+import com.onmoim.server.group.service.GroupService;
+import com.onmoim.server.group.implement.GroupUserQueryService;
+import com.onmoim.server.user.entity.User;
 import com.onmoim.server.user.service.UserQueryService;
 
 import lombok.RequiredArgsConstructor;
@@ -21,6 +30,8 @@ public class ChatMessageFacade {
 
 	private final ChatRoomMemberQueryService chatRoomMemberQueryService;
 	private final UserQueryService userQueryService;
+	private final GroupQueryService groupQueryService;
+	private final GroupUserQueryService groupUserQueryService;
 	private final ChatMessageService chatMessageService;
 
 	/**
@@ -29,14 +40,18 @@ public class ChatMessageFacade {
 	@Transactional
 	public void sendMessage(ChatMessageDto message, Long userId) {
 		Long roomId = message.getRoomId();
-		log.debug("messageDto : {}, sender : {}",message,userId);
+		log.debug("messageDto : {}, sender : {}", message, userId);
 
 		chatRoomMemberQueryService.getByChatRoomIdAndUserId(roomId, userId);
 
 		// 메시지에 인증된 사용자 ID 설정
-		// User user = userQueryService.findById(userId);
+		User user = userQueryService.findById(userId);
+		Group group = groupQueryService.getById(message.getGroupId());
+		GroupUser groupUser = groupUserQueryService.findById(group.getId(), userId)
+			.orElseThrow(() -> new CustomException(ErrorCode.NOT_EXISTS_GROUP));
+
 		message.setSenderId(userId);
-		message.setSenderName("name");
+		message.setChatUserDto(ChatUserDto.create(user, groupUser.isOwner()));
 
 		// 메시지 전송 서비스 호출
 		chatMessageService.sendMessage(message);
