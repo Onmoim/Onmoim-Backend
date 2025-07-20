84c9091db8f3f449a0d3956c6977c770c20b7dbd
baejeu <zeus20240228@gmail.com>
2025-07-20 19:20:33 +0900

    오류 수정

--- src/main/java/com/onmoim/server/group/dto/request/GroupCreateRequestDto.java
+++ src/main/java/com/onmoim/server/group/dto/request/GroupCreateRequestDto.java
@@ -3,9 +3,10 @@ package com.onmoim.server.group.dto.request;
 import static com.onmoim.server.group.dto.request.GroupRequestConstraints.*;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 // 모임 생성 요청
@@ -28,6 +29,7 @@ public record GroupCreateRequestDto (
 	String description,
 
 	@Schema(description = "모임 정원 제약", example = "10")
-	@Size(min = CREATE_MIN_CAPACITY, max = CREATE_MAX_CAPACITY, message = "모임 정원은 5명 이상 300명 이하로 설정해주세요.")
+	@Min(value = CREATE_MIN_CAPACITY, message = "모임 최대 정원은 5명 이상이어야 합니다.")
+	@Max(value = CREATE_MAX_CAPACITY, message = "모임 최대 정원은 300명 이하이어야 합니다.")
 	int capacity
 ) {}
