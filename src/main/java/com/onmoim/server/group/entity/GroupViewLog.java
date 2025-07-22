f7ba2a61523e750fb05d961db307b86807830a28
dbkoo <elqlkoo@gmail.com>
2025-07-23 04:19:46 +0900

    refactor: GroupViewLog 연관관계 JoinColumn 추가

--- src/main/java/com/onmoim/server/group/entity/GroupViewLog.java
+++ src/main/java/com/onmoim/server/group/entity/GroupViewLog.java
@@ -19,9 +19,11 @@ public class GroupViewLog extends BaseEntity {
 	private Long groupViewLogId;
 
 	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
 	private User user;
 
 	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "group_id")
 	private Group group;
 
 	@Column(nullable = false)
