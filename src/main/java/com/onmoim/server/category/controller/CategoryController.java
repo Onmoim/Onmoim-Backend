6569db86d4d4c011da713fe1c34b709b8b9e4ea2
dbkoo <elqlkoo@gmail.com>
2025-07-27 05:00:20 +0900

    refactor: 카테고리별 모임 조회 컨트롤러 변경

--- src/main/java/com/onmoim/server/category/controller/CategoryController.java
+++ src/main/java/com/onmoim/server/category/controller/CategoryController.java
@@ -2,11 +2,12 @@ package com.onmoim.server.category.controller;
 
 import java.util.List;
 
+import com.onmoim.server.common.response.CommonCursorPageResponseDto;
+import com.onmoim.server.group.dto.response.GroupSummaryByCategoryResponseDto;
+import com.onmoim.server.group.service.GroupService;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.onmoim.server.category.dto.CategoryResponseDto;
 import com.onmoim.server.category.service.CategoryQueryService;
@@ -25,10 +26,11 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
-@Tag(name = "Category", description = "카테고리 조회용 공통 API")
+@Tag(name = "Category", description = "카테고리 관련 API")
 public class CategoryController {
 
 	private final CategoryQueryService categoryQueryService;
+	private final GroupService groupService;
 
 	@GetMapping("/v1/category")
 	@Operation(
@@ -48,6 +50,33 @@ public class CategoryController {
 			description = "서버 내부 오류")})
 	public ResponseEntity<ResponseHandler<List<CategoryResponseDto>>> findAllCategories() {
 		List<CategoryResponseDto> response = categoryQueryService.findAllCategories();
+		return ResponseEntity.ok(ResponseHandler.response(response));
+	}
+
+	/**
+	 * 카테고리 - 카테고리별 모임 조회
+	 */
+	@GetMapping("/v1/category/{categoryId}/groups")
+	@Operation(
+		summary = "카테고리 - 카테고리별 모임 조회",
+		description = "카테고리를 조건으로 모임을 조회합니다.")
+	@ApiResponses({
+		@ApiResponse(
+			responseCode = "200",
+			description = "조회 성공",
+			content = @Content(
+				schema = @Schema(implementation = CommonCursorPageResponseDto.class))),
+		@ApiResponse(responseCode = "401", description = "인증 실패")
+	})
+	public ResponseEntity<ResponseHandler<CommonCursorPageResponseDto<GroupSummaryByCategoryResponseDto>>> getGroupsByCategory(
+		@Parameter(description = "카테고리 ID", required = true, in = ParameterIn.PATH)
+		@PathVariable Long categoryId,
+		@RequestParam(required = false)
+		@Parameter(description = "다음 페이지 커서 ID (첫 페이지는 생략, 이전 페이지의 nextCursorId)") Long cursorId,
+		@RequestParam(defaultValue = "10")
+		@Parameter(description = "페이지 크기") int size
+	) {
+		CommonCursorPageResponseDto<GroupSummaryByCategoryResponseDto> response = groupService.getGroupsByCategory(categoryId, cursorId, size);
 		return ResponseEntity.ok(ResponseHandler.response(response));
 	}
 
